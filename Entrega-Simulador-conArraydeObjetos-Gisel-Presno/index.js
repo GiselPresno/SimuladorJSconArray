2764eb65685b5ab1ed3d62be3f3bd28d6ca31b6a
GiselPresno <80177815+GiselPresno@users.noreply.github.com>
2021-08-11 23:37:34 -0300

    Add files via upload

---
+++ Entrega-Simulador-conArraydeObjetos-Gisel-Presno/index.js
@@ -0,0 +1,115 @@
+let nombre = prompt("Ingrese su nombre:")
+
+function Product(id, name, price) {
+    this.id = id
+    this.name = name
+    this.price = price
+}
+
+const products = [
+    {id: 100,
+    name: traducción,
+    precio: 10
+    },
+    {id: 200,
+    name: transcripción,
+    precio: 200
+    },
+    {id: 300,
+    name: subtitulado,
+    precio: 220
+    }
+]
+
+let product1 = new Product(100, "traducción", 10)
+let product2 = new Product(200, "", 200)
+let product3 = new Product(300, "", 220)
+
+let servicio = parseInt(prompt("¡Bienvenid@, "+nombre +"! Ingrese por favor el código del servicio a solicitar (100 para traducciones, 200 para transcripciones y 300 para subtitulados):"))
+
+if (servicio === product1.id) {
+    let palabras = prompt("Indique por favor la cantidad aproximada de palabras a traducir:")
+
+    let resultado = calculadora( palabras, 10, "*")
+
+    alert("El presupuesto aproximado sería de " + resultado + " pesos.")
+
+    function calculadora(primerNumero, segundoNumero, operacion) {
+        switch (operacion) {
+            case "+":
+                return primerNumero + segundoNumero;
+                break;
+            case "-":
+                return primerNumero - segundoNumero;
+                break;
+            case "*":
+                return primerNumero * segundoNumero;
+                Break;
+
+            case "/":
+                return primerNumero / segundoNumero;
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+}else if (servicio === product2.id) {
+    let palabras = prompt("Indique por favor la cantidad aproximada de minutos a transcribir:")
+
+    let resultado = calculadora( palabras, 200, "*")
+
+    alert("El presupuesto aproximado sería de " + resultado + " pesos.")
+
+    function calculadora(primerNumero, segundoNumero, operacion) {
+        switch (operacion) {
+            case "+":
+                return primerNumero + segundoNumero;
+                break;
+            case "-":
+                return primerNumero - segundoNumero;
+                break;
+            case "*":
+                return primerNumero * segundoNumero;
+                Break;
+
+            case "/":
+                return primerNumero / segundoNumero;
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+}else if(servicio === product3.id) {
+    let palabras = prompt("Indique por favor la cantidad aproximada de minutos a subtitular:")
+
+    let resultado = calculadora( palabras, 220, "*")
+
+    alert("El presupuesto aproximado sería de " + resultado + " pesos.")
+
+    function calculadora(primerNumero, segundoNumero, operacion) {
+        switch (operacion) {
+            case "+":
+                return primerNumero + segundoNumero;
+                break;
+            case "-":
+                return primerNumero - segundoNumero;
+                break;
+            case "*":
+                return primerNumero * segundoNumero;
+                Break;
+
+            case "/":
+                return primerNumero / segundoNumero;
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+}else {
+    prompt("Código incorrecto. Intente nuevamente por favor (100 para traducciones, 200 para transcripciones y 300 para subtitulados):")
+}
+
+console.log(resultado)
